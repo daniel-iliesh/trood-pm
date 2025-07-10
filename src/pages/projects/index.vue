52c6f0e41ea8a2f9e3bf5516293a89a0beaf6ae3
Daniel Ilies <iliesh.daniel@gmail.com>
2025-07-10 07:24:46 +0300

    skeleton loaders for projects added

--- src/pages/projects/index.vue
+++ src/pages/projects/index.vue
@@ -1,21 +1,20 @@
 <template>
   <v-container>
     <!-- Section -->
-    <v-row class="flex-column">
+    <v-row class="flex-column" v-if="projects || loading">
       <v-row class="align-center justify-space-between px-4">
         <h1>Active projects</h1>
         <v-btn to="/projects/create"> Create project </v-btn>
       </v-row>
 
       <v-row class="flex-wrap mt-6">
+        <v-col v-if="loading" class="card-col">
+          <v-skeleton-loader type="card"></v-skeleton-loader>
+        </v-col>
         <v-col
+          v-else
           class="card-col"
-          v-for="project in projects.filter((p) => {
-            const currDate = new Date(
-              p.deadline.split('.').reverse().join('.'),
-            ).getTime();
-            return Date.now() < currDate;
-          })"
+          v-for="project in projects"
           :key="project.id"
         >
           <ProjectCard :project="project" />
@@ -23,20 +22,19 @@
       </v-row>
     </v-row>
     <!-- End Section -->
-    <v-row class="flex-column mt-12">
+    <v-row class="flex-column mt-12" v-if="passedProjects || loading">
       <v-row class="align-center justify-space-between px-4">
         <h1>Passed projects</h1>
       </v-row>
 
       <v-row class="flex-wrap mt-6">
+        <v-col v-if="loading" class="card-col">
+          <v-skeleton-loader type="card"></v-skeleton-loader>
+        </v-col>
         <v-col
+          v-else
           class="card-col"
-          v-for="project in projects.filter((p) => {
-            const currDate = new Date(
-              p.deadline.split('.').reverse().join('.'),
-            ).getTime();
-            return Date.now() > currDate;
-          })"
+          v-for="project in passedProjects"
           :key="project.id"
         >
           <ProjectCard :project="project" />
@@ -52,6 +50,7 @@ import { getProjects } from "@/api/projects";
 import { Project } from "@/api/dtos";
 import ProjectCard from "@/components/ProjectCard.vue";
 
+const passedProjects: Ref<Project[]> = ref([]);
 const projects: Ref<Project[]> = ref([]);
 const loading = ref(false);
 
@@ -59,7 +58,20 @@ onMounted(() => {
   loading.value = true;
   getProjects()
     .then((res) => {
-      projects.value = res;
+      // TODO: Change the backend to use ISO 8601 format
+      projects.value = res.filter((p) => {
+        const currDate = new Date(
+          p.deadline.split(".").reverse().join("."),
+        ).getTime();
+        return Date.now() < currDate;
+      });
+
+      passedProjects.value = res.filter((p) => {
+        const currDate = new Date(
+          p.deadline.split(".").reverse().join("."),
+        ).getTime();
+        return Date.now() > currDate;
+      });
     })
     .finally(() => {
       loading.value = false;
